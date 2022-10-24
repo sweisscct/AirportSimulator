f116ccf5e34a09cf3c145fb1b74ce0f69a32de64
Sam Weiss <114678287+sweisscct@users.noreply.github.com>
2022-10-24 12:52:33 +0100

    Initial commit

---
+++ src/airportsimulator/AirportSimulator.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package airportsimulator;
+
+/**
+ *
+ * @author Sam Weiss
+ * @email; sweiss@cct.ie
+ */
+public class AirportSimulator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+    
+}
